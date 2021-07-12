791719c81afc4def65d35fbb10cc2806e9ea5f9e
pavloskii <pavlo.igor@gmail.com>
2021-07-12 23:13:31 +0200

    Finished dodge the creeps

---
+++ dodge-the-creeps/Mob.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Mob : RigidBody2D
+{
+	// Don't forget to rebuild the project so the editor knows about the new export variables.
+
+	[Export]
+	public int MinSpeed = 150; // Minimum speed range.
+
+	[Export]
+	public int MaxSpeed = 250; // Maximum speed range.
+	
+	// C# doesn't implement GDScript's random methods, so we use 'System.Random' as an alternative.
+	static private Random _random = new Random();
+
+	public override void _Ready()
+	{
+		var animSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+		var mobTypes = animSprite.Frames.GetAnimationNames();
+		animSprite.Animation = mobTypes[_random.Next(0, mobTypes.Length)];
+	}
+	
+	public void OnVisibilityNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
+}
